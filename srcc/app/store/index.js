6488fa5cde2cfaedac2ac5a34e491a256b26a85b
NAJIB <najib.azmi2019@gmail.com>
2024-09-17 21:51:02 +0100

    src dossier created

--- srcc/app/store/index.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import WeatherSlice from "../../features/weather/WeatherSlice";
-
-export const store=configureStore({
-    reducer:{
-           Weather:WeatherSlice         
-    }                
-})
